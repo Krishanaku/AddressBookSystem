e9b049e160f2d4ab67f12e4754844c01f52e23f6
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-02-17 13:29:15 +0530

    Add : Ability to create a Contacts in Address Book with first and last names, address, city, state, zip, phone number and email...

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to AddressBook Program ");
+           AddressBookMain c1 = new AddressBookMain("Krishna", "Kumar", "abc@gmail.com", "Mahjara", "nagaland", "sikkim", 440023, 1234567890);
         }
     }
 }

4c26e144e118e9f68579eb0d723799b1126892a7
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-03-06 13:02:41 +0530

    Add : Ability to ensure there is no Duplicate Entry of the same Person in a particular Address Book - Duplicate Check is done on Person Name while adding person to Address Book. - Use Collection Methods to Search Person by Name for Duplicate Entry - Override equals method to check for Duplicate

--- Program.cs
+++ Program.cs
@@ -4,7 +4,32 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to AddressBook Program ");
+            //Console.WriteLine("Welcome to AddressBook Program ");
+
+            //AddressBookMain.AddContact("krishna");
+            //AddressBookMain.Display();
+            //AddressBookMain.EditConatct();
+            //AddressBookMain.DeleteContact("krishna");
+            //AddressBookMain.AddMultipleConatcts("krihsn");
+
+            List<Person> people = new List<Person>();
+            people.Add(new Person { firstName = "Sagar", lastName = "Shahu", city = "Nagpur", state = "Maharashtra", zipcode = "440023", country = "India", phone = "55455665", email = "shahu@gmail.com" });
+
+            Person newEntry1 = new Person { firstName = "Sagar", lastName = "Shahu", city = "Nagpur", state = "Maharashtra", zipcode = "440023", country = "India", phone = "55455665", email = "shahu@gmail.com" };
+            if (people.Any(e => e.firstName == newEntry1.firstName))
+            {
+                Console.WriteLine("Duplicate entry: {0}", newEntry1.firstName);
+            }
+            else
+            {
+                people.Add(newEntry1);
+            }
+
+
+
+
+
+
         }
     }
 }
